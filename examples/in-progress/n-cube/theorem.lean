31f6c1e1eff591bb504c5663b6f7962057152926
Augustindou <augustin.doultremontao@gmail.com>
2022-06-21 18:16:36 +0200

    new example

---
+++ examples/in-progress/n-cube/theorem.lean
@@ -0,0 +1,29 @@
+import LeanUtils
+open Nat
+
+theorem n_cube_plus_2_n (n : Nat) : divisible 3 (n^3 + 2*n) := by
+  match n with
+  | 0 => repeat (first | rfl | trivial | ring | simp_all)
+  | k+1 => 
+    have h₁ : divisible 3 (k ^ 3 + 2 * k) := n_cube_plus_2_n k
+
+    have ⟨z, h₂⟩ : ∃ z, k^3 + 2*k = 3*z := by
+      repeat (first | ring | simp_all)
+    
+    have h₃ : (k + 1) ^ 3 + 2 * (k + 1) = 3*(z + k^2 + k + 1) := by
+      calc
+        (k + 1) ^ 3 + 2 * (k + 1) = k^3 + 3*k^2 + 5*k + 3 := by 
+          repeat (first | ring | simp_all)
+        _ = k^3 + 2*k + 3 * (k^2 + k + 1) := by 
+          repeat (first | ring | simp_all)
+        _ = 3*z + 3 * (k^2 + k + 1) := by
+          repeat (first | ring | simp_all)
+        _ = 3 * (z + k^2 + k + 1) := by
+          repeat (first | ring | simp_all)
+    try rw [h₃]
+  
+    apply mod_rewrite.mpr
+    exact ⟨_, by repeat (first | rfl | assumption | ring | simp_all)⟩
+
+  
+
