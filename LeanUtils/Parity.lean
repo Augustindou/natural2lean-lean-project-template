45c3e2e8593accde231bc2af25da244bdd3d2b67
Augustindou <augustin.doultremontao@gmail.com>
2022-06-23 12:51:41 +0200

    corrections

--- LeanUtils/Parity.lean
+++ LeanUtils/Parity.lean
@@ -1,34 +1,32 @@
-import LeanUtils.div
+import LeanUtils.Div
 /- Parity : functions and theorems related to parity -/
 
 namespace Nat
-  def even (a : Nat) : Prop := ∃ (n : Nat), a = 2 * n
-  def odd (a : Nat) : Prop := ∃ (n : Nat), a = 2 * n + 1
+  def even (a : Nat) : Prop := a % 2 = 0
+  def odd (a : Nat) : Prop := a % 2 = 1
 
-  theorem even_rewrite {a : Nat} : even a ↔ a % 2 = 0 := 
+  theorem even_rewrite {a : Nat} : even a ↔ ∃ (n : Nat), a = 2 * n := 
     Iff.intro
+      (by
+        intro h
+        rw [even, mod_rewrite] at h
+        simp_all)
       (by 
         intro h
         have ⟨n, hn⟩ := h
-        rw [hn, mod_rewrite]
-        exact ⟨n, by simp⟩)
-      (by
-        intro h
-        rw [even]
-        rw [mod_rewrite] at h
-        simp_all)
+        rw [hn, even, mod_rewrite]
+        exact ⟨_, by rfl⟩)
   
-  theorem odd_rewrite {a : Nat} : odd a ↔ a % 2 = 1 :=
+  theorem odd_rewrite {a : Nat} : odd a ↔ ∃ (n : Nat), a = 2 * n + 1 :=
     Iff.intro
+      (by
+        intro h
+        rw [odd, mod_rewrite] at h
+        simp_all)
       (by 
         intro h
         have ⟨n, hn⟩ := h
-        rw [hn, mod_rewrite]
-        exact ⟨n, by simp⟩)
-      (by
-        intro h
-        rw [odd]
-        rw [mod_rewrite] at h
-        simp_all)
+        rw [hn, odd, mod_rewrite]
+        exact ⟨_, by rfl⟩)
 
 end Nat
