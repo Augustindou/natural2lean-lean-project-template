e96b71761bf53e19c2b3454aa538a8b9d36bd77a
Augustindou <augustin.doultremontao@gmail.com>
2022-06-23 12:27:04 +0200

    even and odd rewrite

--- LeanUtils/Parity.lean
+++ LeanUtils/Parity.lean
@@ -1,6 +1,34 @@
+import LeanUtils.div
 /- Parity : functions and theorems related to parity -/
 
 namespace Nat
   def even (a : Nat) : Prop := ∃ (n : Nat), a = 2 * n
   def odd (a : Nat) : Prop := ∃ (n : Nat), a = 2 * n + 1
+
+  theorem even_rewrite {a : Nat} : even a ↔ a % 2 = 0 := 
+    Iff.intro
+      (by 
+        intro h
+        have ⟨n, hn⟩ := h
+        rw [hn, mod_rewrite]
+        exact ⟨n, by simp⟩)
+      (by
+        intro h
+        rw [even]
+        rw [mod_rewrite] at h
+        simp_all)
+  
+  theorem odd_rewrite {a : Nat} : odd a ↔ a % 2 = 1 :=
+    Iff.intro
+      (by 
+        intro h
+        have ⟨n, hn⟩ := h
+        rw [hn, mod_rewrite]
+        exact ⟨n, by simp⟩)
+      (by
+        intro h
+        rw [odd]
+        rw [mod_rewrite] at h
+        simp_all)
+
 end Nat
--- LeanUtils/Tactic.lean
+++ LeanUtils/Tactic.lean
@@ -2,8 +2,33 @@ import LeanUtils.Div
 import LeanUtils.Parity
 
 attribute [simp]
+  -- -- addition
+  --     -- swapping
+  --     Nat.add_assoc
+  --     Nat.add_comm
+  --     Nat.add_left_comm
+  --   -- multiplication
+  --     -- swapping
+  --     Nat.mul_assoc
+  --     Nat.mul_comm
+  --     Nat.mul_left_comm
+  --     -- zero & one
+  --     Nat.mul_zero
+  --     Nat.zero_mul
+  --     Nat.mul_one
+  --     Nat.one_mul
+  --   -- powers
+  --     -- zero & one
+  --     Nat.pow_zero
+  --     -- succ
+  --     Nat.pow_succ
+  --   -- multiplication to addition
+  --   Nat.left_distrib
+  --   Nat.right_distrib
   -- defined in LeanUtils
   Nat.divisible
   Nat.mod_rewrite
   Nat.even
+  Nat.even_rewrite
   Nat.odd
+  Nat.odd_rewrite
